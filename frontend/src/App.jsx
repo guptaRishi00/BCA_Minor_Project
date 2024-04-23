03ef36e8dab3e4b1dd1f30517fdb5a5a82acaf71
guptaRishi00 <rishigupta54066@gmail.com>
2024-04-24 03:11:23 +0530

    Not Much Just Added A Logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="bg-red-50">
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default Navbar;
